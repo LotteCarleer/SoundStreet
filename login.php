7e9465989db944bba5ae58bbc40ec82815219f20
Lotte Carleer <lottecarleer@LaptopvanLotte.lan>
2025-11-23 20:26:46 +0100

    feat: html van login.php aangemaakt

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login
+    </title>
+</head>
+<body>
+    <h2>Inloggen</h2>
+
+    <form method="post" action="">
+        <label for="email">Email:</label> <br>
+        <input type="email" name="email" required>
+        <br><br>
+
+        <label for="wachtwoord">Wachtwoord:</label> <br>
+        <input type="password" name="password" required>
+
+       <br><br>
+        <button type="submit">Log in</button>
+    </form>
+</body>
+</html>

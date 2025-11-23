58b36922cff72d5a070966c6f3d56cc3cdbaf94e
Lotte Carleer <lottecarleer@LaptopvanLotte.lan>
2025-11-23 23:26:12 +0100

    feat: public function aangemaakt

--- classes/Product.php
+++ classes/Product.php
@@ -6,6 +6,16 @@ class Product {
     public function __construct($db){
         $this->db = $db; 
     }
+
+    public function all($category_id = null){
+        if ($category_id){
+            $stmt = $this->db->prepare("SELECT * FROM products WHERE category_id = ?");
+            $stmt->execute([$category_id]);
+        } else {
+            $stmt = $this->db->query("SELECT * FROM products");
+        }
+        return $stmt->fetchAll();
+    }
 }
 
 

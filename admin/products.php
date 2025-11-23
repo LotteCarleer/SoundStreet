d47b204103e70f6c39f3d082edb1a94f68971c93
Lotte Carleer <lottecarleer@LaptopvanLotte.lan>
2025-11-23 23:08:06 +0100

    feat: html admin

---
+++ admin/products.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Categorie toevoegen</title>
+</head>
+<body>
+    <h2>Categorie toevoegen</h2>
+    <form method="POST">
+
+      <input type="text" name="category_name" placeholder="Naam nieuwe categorie">
+      <button name="add_category">Toevoegen</button>
+
+    </form>
+     <h2>Product toevoegen</h2>
+     <form method="POST" enctype="multipart/form-data">
+        <input type="text" name="title" placeholder="Titel">
+        <textarea name="description" placeholder="Beschrijving"></textarea>
+        <input type="number" name="price" placeholder="Prijs" >
+     </form>
+
+    <input type="file" name="image" >
+    <button name="addProduct">Toevoegen</button>
+</body>
+</html>

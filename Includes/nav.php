1f9e6e2239987538e37314497f3c8af480ebf5d5
Lotte Carleer <lottecarleer@LaptopvanLotte.lan>
2025-11-24 00:15:56 +0100

    feat: begin css navigatie

--- Includes/nav.php
+++ Includes/nav.php
@@ -6,14 +6,16 @@ if (session_status() === PHP_SESSION_NONE){
 
 ?>
 
-<nav style="padding: 10px; background:#eee; margin-bottom:20px;">
+<nav style="padding: 10px; background:#F2E9E1; margin-bottom:20px;">
 
-<a href="index.php">Home</a>
-<a href="product.php">Products</a>
+<a href="index.php" style="margin-right: 15px;">Home</a>
+<a href="product.php" style="margin-right: 15px;">Products</a>
 
-<a href="">Mijn account</a>
+<a href="" style="margin-right: 15px;">Mijn account</a>
 
+<?php if(!empty($_SESSION["is_admin"]) && $_SESSION["is_admin"] === true): ?>
 <a href="/admin/products.php">Admin Panel</a>
+<?php endif; ?>
 
 <form action="/search.php" method="GET">
     <input type="text" name="q" placeholder="Zoek" required>

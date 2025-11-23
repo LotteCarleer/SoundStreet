46455b02c7438dda462e4e873076a49faa56c483
Lotte Carleer <lottecarleer@LaptopvanLotte.lan>
2025-11-23 23:50:18 +0100

    feat: navigatie aangemaakt

--- Includes/nav.php
+++ Includes/nav.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<nav>
+
+<a href="index.php">Home</a>
+<a href="product.php">Products</a>
+
+<a href="">Mijn account</a>
+
+<a href="/admin/products.php">Admin Panel</a>
+
+<form action="/search.php" method="GET">
+    <input type="text" name="q" placeholder="Zoek" required>
+    <button type="submit">Zoeken</button> 
+</form>
+</nav>

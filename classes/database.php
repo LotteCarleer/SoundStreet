7c722b83cc74b6563fca0ca932b07939c6ff6051
Lotte Carleer <lottecarleer@LaptopvanLotte.lan>
2025-11-23 22:44:16 +0100

    feat: database gelinkt

---
+++ classes/database.php
@@ -0,0 +1,21 @@
+<?php
+
+class Database {
+    private $pdo;
+
+    public function __construct(){
+        $this->pdo = new PDO ('mysql:host=localhost;dbname=SoundStreet', "root", "");
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function query ($sql){
+        return $this->pdo->query($sql);
+    }
+
+    public function prepare($sql){
+        return $this->pdo->prepare($sql);
+    }
+}
+
+
+?>

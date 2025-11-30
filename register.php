bf7c66c54a4938c8f9be946b24c5d8409e20c036
Lotte Carleer <lottecarleer@LaptopvanLotte.lan>
2025-11-30 17:54:58 +0100

    feat:made html for register.php

---
+++ register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registreren</title>
+</head>
+<body>
+
+<h2>Registreren</h2>
+
+<form method="POST">
+    <input type="text" name="username" placeholder="Gebruikersnaam">
+    <input type="email" name="email" placeholder="Email" >
+    <input type="password" name="password" placeholder="Wachtwoord">
+    <button type="submit">Registreren</button>
+</form>
+
+<p>Heb je al een account? <a href="login.php">Log in</a></p>
+</body>
+</html>

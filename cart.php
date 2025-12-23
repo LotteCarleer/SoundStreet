49360633004863b6a92aa4eb233c43056e823819
Lotte Carleer <lottecarleer@Laptop-van-Lotte.local>
2025-12-23 23:55:24 +0100

    feat: wrote html of cart.php

---
+++ cart.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Winkelmandje</title>
+</head>
+<body>
+
+<h2>Winkelmandje</h2>
+
+<p>Je winkelmandje is voorlopig leeg</p>
+
+<a href="product.php">Verder winkelen</a>
+    
+</body>
+</html>

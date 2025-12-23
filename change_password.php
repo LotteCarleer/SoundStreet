7c5149f3e49ef4a1e2d16a1be5c35a5a81251194
Lotte Carleer <lottecarleer@Laptop-van-Lotte.local>
2025-12-23 13:55:27 +0100

    feat: made html for change password

---
+++ change_password.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wachtwoord wijzigen</title>
+</head>
+<body>
+
+<h2>Wachtwoord wijzigen</h2>
+
+<a href="account.php">Ga terug</a>
+
+<form method="POST">
+    <label for="current_password">Huidig wachtwoord:</label>
+    <input type="password" id="current_password" name="current_password"><br><br>
+
+    <label for="new_password">Nieuw wachtwoord:</label>
+    <input type="password" id="new_password" name="new_password"><br><br>
+
+    <button type="submit" >Wachtwoord wijzigen</button>
+
+
+
+
+</form>
+    
+</body>
+</html>

df7b9c9cbc989837bde75bc4e1cd5b9d2852f3ca
Lotte Carleer <lottecarleer@Laptop-van-Lotte.local>
2025-12-27 00:23:29 +0100

    feat: wrote html for orders.php

---
+++ orders.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mijn bestellingen</title>
+</head>
+<body>
+
+<h1>Mijn bestellingen</h1>
+
+<p>Je hebt nog geen bestellingen geplaatst</p>
+
+<div>
+
+<h3>Bestelling</h3>
+<p>Datum:</p>
+<p>Totaal:</p>
+
+<h4>Artikelen</h4>
+
+<ul>
+
+<li>
+"product and price"
+</li>
+
+
+
+</ul>
+    
+
+</div>
+
+<a href="account.php">Terug naar account</a>
+    
+</body>
+</html>

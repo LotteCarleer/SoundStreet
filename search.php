96d7f537418c017efeb7e3b12a9894075e648e1c
Lotte Carleer <lottecarleer@Laptop-van-Lotte.local>
2025-12-29 19:18:47 +0100

    feat: wrote html for search.php

---
+++ search.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zoeken</title>
+</head>
+<body>
+
+<h1>Zoekresultaten voor ...</h1>
+
+<p>Geen producten gevonden</p>
+
+<div class="product">
+
+<h3>title</h3>
+<p>artist</p>
+<p> ... SoundCoins</p>
+<a href="">Bekijk details</a>
+
+</div>
+
+<a href="product.php">Terug naar producten</a>
+    
+</body>
+</html>

216a5d7e5dc7eddb33ffa873e27dc04c31bbe4ba
Lotte Carleer <lottecarleer@Laptop-van-Lotte.local>
2025-12-30 17:39:04 +0100

    fix: changed classes/database.php for render

--- classes/database.php
+++ classes/database.php
@@ -5,14 +5,14 @@ class Database {
 
     public function __construct(){
 
-        $host = $_ENV["DB_HOST"] ?? "localhost";
-        $db = $_ENV["DB_NAME"] ?? "SoundStreet";
-        $user = $_ENV["DB_USER"] ?? "root";
-        $pass = $_ENV["DB_PASS"] ?? "";
+        $host = getenv("DB_HOST") ?: "localhost";
+        $db = getenv("DB_NAME") ?: "SoundStreet";
+        $user = getenv("DB_USER") ?: "root";
+        $pass = getenv("DB_PASS") ?: "";
 
 
 
-        $this->pdo = new PDO ("mysql:host=$host;dbname=$db; charset=utf8", $user , $pass);
+        $this->pdo = new PDO ("mysql:host=$host;dbname=$db;charset=utf8mb4", $user , $pass);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 

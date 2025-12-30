16069270a4987a760ecce82efe54879efb3c49a8
Lotte Carleer <lottecarleer@Laptop-van-Lotte.local>
2025-12-30 17:56:44 +0100

    fix: changed database.php to old code

--- classes/database.php
+++ classes/database.php
@@ -4,15 +4,7 @@ class Database {
     private $pdo;
 
     public function __construct(){
-
-        $host = getenv("DB_HOST") ?: "localhost";
-        $db = getenv("DB_NAME") ?: "SoundStreet";
-        $user = getenv("DB_USER") ?: "root";
-        $pass = getenv("DB_PASS") ?: "";
-
-
-
-        $this->pdo = new PDO ("mysql:host=$host;dbname=$db;charset=utf8mb4", $user , $pass);
+        $this->pdo = new PDO ('mysql:host=localhost;dbname=SoundStreet', "root", "");
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 

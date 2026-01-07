5186cc794c7e4f4d1fb2dee64782e92b223fc6b2
Lotte Carleer <lottecarleer@LaptopvanLotte.lan>
2026-01-07 18:21:36 +0100

    fix: changed language type from en to nl

--- account.php
+++ account.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true ){
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
